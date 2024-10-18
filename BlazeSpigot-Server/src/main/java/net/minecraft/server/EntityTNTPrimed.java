2cf9b687b6898aafa141d3588e2ade64487d0f0d
ruhanslater2002 <ruhan.slater@gmail.com>
2024-10-18 16:05:35 +0200

    Issues fixes

--- BlazeSpigot-Server/src/main/java/net/minecraft/server/EntityTNTPrimed.java
+++ BlazeSpigot-Server/src/main/java/net/minecraft/server/EntityTNTPrimed.java
@@ -13,7 +13,7 @@ public class EntityTNTPrimed extends Entity {
     private EntityLiving source;
 
     // The explosion power (radius), set to 4 by default.
-    public float yield = 5; // CraftBukkit - added field
+    public float yield = 4; // CraftBukkit - added field
 
     // Determines whether the explosion will cause fire.
     public boolean isIncendiary = false; // CraftBukkit - added field
@@ -129,7 +129,7 @@ public class EntityTNTPrimed extends Entity {
         } else {
             // Update the entity (collision checks, etc.) and spawn smoke particles.
             this.W();
-            this.world.addParticle(EnumParticle.SMOKE_NORMAL, this.locX, this.locY + 0.5D, this.locZ, 0.0D, 0.0D, 0.0D, Constants.EMPTY_ARRAY);
+//            this.world.addParticle(EnumParticle.SMOKE_NORMAL, this.locX, this.locY + 0.5D, this.locZ, 0.0D, 0.0D, 0.0D, Constants.EMPTY_ARRAY);
         }
     }
 
